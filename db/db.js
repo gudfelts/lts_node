8a650c2bdd5a4f24a3d66d2835792910b927ce9e
Roxk-33 <xiaorock@126.com>
2017-10-31 16:02:26 +0800

    fix

--- db/db.js
+++ db/db.js
@@ -1,29 +1,64 @@
 const mysql  = require('mysql');
 const config = require('config-lite')(__dirname);
-const pool   = mysql.createPool({
-  connectionLimit : 100,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'lts'
-});
+// const pool   = mysql.createPool({
+//   connectionLimit : 100,
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'root',
+//   database : 'lts'
+// });
 
+// let query = function(sql,options,callback){  
+//   pool.getConnection(function(err,conn){  
+//       if(err){  
+//           callback(err,null,null);  
+//       }else{  
+//           conn.query(sql,options,function(err,results){  
+//               //释放连接 
+  
+//               // conn.release();
+//               conn.end();
+//               if(err) throw err;  
+//               //事件驱动回调  
+//               callback(err,results);  
+//           });  
+//       }  
+//   }); 
+// };  
+
+function handleError (err) {
+  if (err) {
+    // 如果是连接断开，自动重新连接
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      connect();
+    } else {
+      console.error(err.stack || err);
+    }
+  }
+}
+
+// 连接数据库
+function connect () {
+  db = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'root',
+        database : 'lts'
+  });
+  db.connect(handleError);
+  db.on('error', handleError);
+}
+
+var db;
+connect();
 let query = function(sql,options,callback){  
-  pool.getConnection(function(err,conn){  
-      if(err){  
-          callback(err,null,null);  
-      }else{  
-          conn.query(sql,options,function(err,results){  
+  db.query(sql,options,function(err,results){  
               //释放连接 
   
               // conn.release();
-              conn.end();
               if(err) throw err;  
               //事件驱动回调  
               callback(err,results);  
           });  
       }  
-  }); 
-};  
-
 module.exports ={ query }; 
