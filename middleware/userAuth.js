f52c759050838efb651357dba05e7b3ef9605be4
Roxk-33 <xiaorock@126.com>
2017-12-02 17:46:52 +0800

    fix

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,11 +1,9 @@
 module.exports = (ctx,next)=>{
     
     if(ctx.session.isLogin){
-    console.log(ctx.session.user)
     
         return next();
     }else{
-        console.log("未登录")
         ctx.response.body = {
             code: 500,
             msg: "未登录!"
